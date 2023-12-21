3e7a5101426286ca15a7d9c68901c9bd43a91a46
HQLoke <hqloke007@gmail.com>
2023-12-21 12:46:11 +0800

    Created a cli command for selective scraping or all at once.

---
+++ outlet_locator/outlets/management/commands/scrape_outlets.py
@@ -0,0 +1,12 @@
+from unittest.mock import Base
+from django.core.management.base import BaseCommand, CommandError, CommandParser
+
+from . import web_scraper
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('states', nargs='*', type=str, help='Choosing state (optional)')
+    
+    def handle(self, *args, **options):
+        states = options['states']
+        web_scraper.scrape_website_data(states)
